afc7c0bc0d8312ce7fc54053f25142fb7c48b7a8
erik-haggstrom <erik.haggstrom0@gmail.com>
2023-09-21 21:40:43 +0200

    did the basic functions

---
+++ java.js
@@ -0,0 +1,15 @@
+function add (a, b) {
+  return a + b;
+}
+
+function subb (a, b) {
+  return a - b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
+
+function div (a, b) {
+  return a / b;
+}
